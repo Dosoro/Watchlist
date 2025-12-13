449f66c3403499615834e4fb3d9d3212e00a2373
Dosoro <alibakirwork@gmail.com>
2025-12-14 00:39:34 +0300

    feat: add auth routes and integratee with server

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,8 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/api/health", (req, res) => {
   res.json({ message: "Server is running" });
